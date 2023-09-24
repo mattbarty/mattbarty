24c1355c3e564f244bbc0b9ac6c077c888f2c386
Matt Barty <Matthew.barty@outlook.com>
2023-09-24 14:13:31 +0100

    Redesigned in dev banner

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -3,19 +3,6 @@ const Banner = () => {
     <div className="w-full max-w-4xl flex justify-center my-4">
       <div className="flex w-full h-full items-center mx-4">
         <div className="flex flex-col w-full">
-          <div className="flex flex-col items-center bg-yellow-100 py-4 border border-yellow-600">
-            <div className="font-bold text-2xl text-red-800 my-2">
-              <div>
-                🚧 In Development 🚧</div>
-            </div>
-            <div>I&apos;m still building this site - In the meantime, most my projects are on my ➡️ <a href='https://www.linkedin.com/in/matthew-barty/' className="font-bold underline-offset-4 underline text-blue-500">LinkedIn</a> ⬅️</div>
-          </div>
-          <div className="text-4xl my-2">
-            Matthew Barty
-          </div>
-          <div className="text-xl font-semithin">
-            UX Engineer || Creative Technologist
-          </div>
         </div>
       </div>
     </div >
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,22 +2,35 @@ import Link from "next/link";
 
 const Banner = () => {
   return (
-    <div className="bg-gray-200 h-12 w-full flex justify-center">
-      <div className="max-w-4xl flex justify-between  w-full px-12">
-        <div className="flex w-full h-full items-center">
-          <div className="w-full">
-          </div>
-          <div className="flex">
-            <Link href='/'>
-              <div className="max-4">Home</div>
-            </Link>
-            <Link href='/about'>
-              <div className="mx-4">About</div>
-            </Link>
+    <>
+      <div className="bg-gray-200 h-auto w-full flex justify-center">
+        <div className="max-w-4xl flex justify-around md:justify-between w-full px-4 md:px-12">
+          <div className="flex w-full h-full items-center">
+            <div className="w-full">
+              <div className="my-2 flex flex-col">
+                <span className="font-bold text-lg mx-2">Matthew Barty</span>
+                <span className="font-thin text-sm mx-2">UX Engineer || Creative Technologist</span>
+              </div>
+            </div>
+            <div className="flex">
+              <Link href='/'>
+                <div className="mx-2">Projects</div>
+              </Link>
+              <Link href='/about'>
+                <div className="mx-2">About</div>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      {/* In development banner */}
+      <div className="flex flex-col items-center bg-yellow-100 border border-yellow-600 w-full">
+        <div className="text-red-800 my-1">
+          <span className="font-bold">🚧 In Development 🚧 </span>
+          <span className="">-- In the meantime check out my ➡️ <a href='https://www.linkedin.com/in/matthew-barty/' className="font-bold underline-offset-4 underline text-blue-500">LinkedIn</a></span>
+        </div>
+      </div>
+    </>
   );
 };
 
