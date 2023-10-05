06499561f01c7047caa4aa7e7ed5d5b11ca00919
Matt Barty <Matthew.barty@outlook.com>
2023-10-05 12:24:29 +0100

    Added open in new window icon to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -74,13 +74,11 @@ function MoonIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
 }
 
 
-function OpenFileIcon({ ...props }: React.ComponentPropsWithoutRef<'svg'>) {
+function OpenFileIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
       height="1em"
       viewBox="0 0 512 512"
-      aria-hidden="true"
       {...props}>
       <path
         d="M352 0c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9L370.7 96 201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L416 141.3l41.4 41.4c9.2 9.2 22.9 11.9 34.9 6.9s19.8-16.6 19.8-29.6V32c0-17.7-14.3-32-32-32H352zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"
@@ -158,9 +156,9 @@ function MobileNavigation(
                 <a href='/docs/matthew_barty_resume_2023.pdf'
                   target="_blank"
                   rel="noopener noreferrer"
-                  className='relative py-2 transition hover:text-teal-500 dark:hover:text-teal-400 flex'>
+                  className='relative py-2 transition flex items-center'>
                   Resume
-                  {/* <OpenFileIcon className='h-4 w-4 text-zinc-500 dark:text-zinc-400' fill={``} /> */}
+                  <OpenFileIcon className='h-3 w-3 transition fill-zinc-800 dark:fill-zinc-100 ml-2 ' />
                 </a>
                 {/* <MobileNavItem href="/speaking">Speaking</MobileNavItem>
                 <MobileNavItem href="/uses">Uses</MobileNavItem> */}
@@ -211,12 +209,15 @@ function DesktopNavigation(props: React.ComponentPropsWithoutRef<'nav'>) {
         <NavItem href="/about">About</NavItem>
         {/* <NavItem href="/articles">Articles</NavItem> */}
         <NavItem href="/projects">Projects</NavItem>
-        <a href='/docs/matthew_barty_resume_2023.pdf'
+        <a
+          href='/docs/matthew_barty_resume_2023.pdf'
           target="_blank"
           rel="noopener noreferrer"
-          className='relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400'>
+          className='group relative flex px-3 py-2 transition items-center hover:text-teal-500 dark:hover:text-teal-400 '>
           Resume
+          <OpenFileIcon className='h-3 w-3 transition fill-zinc-800 dark:fill-zinc-200 ml-2 group-hover:fill-teal-500 dark:group-hover:fill-teal-400' />
         </a>
+
         {/* <NavItem href="/speaking">Speaking</NavItem>
         <NavItem href="/uses">Uses</NavItem> */}
       </ul>
